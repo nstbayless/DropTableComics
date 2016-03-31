c15159e9b3cecdf0a827188f5505589a3cf287a2
Edward Lee <edwardjihunlee@gmail.com>
2016-03-30 20:51:38 -0700

    fixed dropdown menu in viewcomic with z-index

--- public/javascripts/jquery_contoller.js
+++ public/javascripts/jquery_contoller.js
@@ -19,3 +19,5 @@ $('#myTabs a[href="#profile"]').tab('show') // Select tab by name
 $('#myTabs a:first').tab('show') // Select first tab
 $('#myTabs a:last').tab('show') // Select last tab
 $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+
+$('.dropdown-toggle').dropdown()
